812d05c5f7002c5e693c5d1abb56ee210cdf7922
GASROT <guilhermedeearaujo@gmail.com>
2026-05-13 09:05:17 -0300

    feat(api): configura CORS com opções específicas para o frontend

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ import clientesRoutes from './routes/cliente.js';
 
 const app = express();
 
-app.use(cors());
+// Configuração do CORS
+const corsOptions = {
+    origin: 'https://oficina-front-gules.vercel.app', // Substitua pela URL real do seu frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],    // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Headers permitidos
+    optionsSuccessStatus: 200 
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/cliente', clientesRoutes);
@@ -14,9 +22,7 @@ app.get('/', (req, res) => {
 });
 
 if (process.env.NODE_ENV !== 'test') {
-
     const PORT = process.env.PORT || 3000;
-
     app.listen(PORT, () => {
         console.log(`Servidor rodando na porta ${PORT}`);
     });
